f759f8c853f6c56909fc4673e446dea2d347eeac
ramugs <ramugs004@gmail.com>
2023-11-11 22:16:35 +0530

    updated in response

--- controllers/admin.js
+++ controllers/admin.js
@@ -2,7 +2,7 @@ const createAdmin = (req, res) => {
   res.send("create admin");
 };
 const login = (req, res) => {
-  res.send("login admin");
+  res.send("login admin updated");
 };
 
 module.exports = { createAdmin, login };
