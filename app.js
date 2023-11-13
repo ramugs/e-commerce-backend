3942ea13bc4e1564472f115e4120c0ce009eb6ec
ramugs <ramugs004@gmail.com>
2023-11-13 16:08:13 +0530

    cors security packages

--- app.js
+++ app.js
@@ -6,7 +6,23 @@ const app = express();
 const connectDB = require("./db/connect");
 const adminRouter = require("./routes/admin");
 
+// extra security package
+const helmet = require("helmet");
+const cors = require("cors");
+const xss = require("xss-clean");
+const rateLimiter = require("express-rate-limit");
+
+app.set("trust proxy", 1);
+app.use(
+  rateLimiter({
+    windowMs: 15 * 60 * 1000,
+    max: 100,
+  })
+);
 app.use(express.json());
+app.use(helmet());
+app.use(cors());
+app.use(xss());
 
 app.use("/ecommerce/admin", adminRouter);
 
